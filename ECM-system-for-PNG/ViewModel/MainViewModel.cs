39cf90d4e4e8d7753714d74eb8c95ec5c651fdf6
ReinFlat <tomato535@yandex.ru>
2024-02-13 23:27:22 +0500

    Заменил все кнопки навигации с изображений на вектора добавил стили кнопок, обработал реакцию на наведение, нажатие и выбор Выровнял вёрстку

---
+++ ECM-system-for-PNG/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.Mvvm;
+using Microsoft.Win32;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace ECM_system_for_PNG.ViewModel
+{
+
+}
--- ECM-system-for-PNG/obj/Debug/ECM-system-for-PNG_Content.g.i.cs
+++ ECM-system-for-PNG/obj/Debug/ECM-system-for-PNG_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("vector.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("pics/settings.svg")]
 
 
